eef37ee4bfeda8b28bff5307154424bfd798b6a1
hunjuly <hunjuly@gmail.com>
2023-09-11 08:56:18 +0000

    .

--- scripts/init_database.sh
+++ scripts/init_database.sh
@@ -1,52 +1,11 @@
 #!/bin/bash
-set -e
 cd "$(dirname "$0")"
 cd ..
 . ./.env.development
 
-POSTGRES_IMAGE="postgres:15-alpine3.18"
-
-start_postgresql() {
-    local port=5432
-
-    while :; do
-        docker rm -f $TYPEORM_HOST >/dev/null 2>&1
-
-        set +e
-        output=$(docker run --rm -d --network $PROJECT_NAME \
-            --name $TYPEORM_HOST \
-            -e POSTGRES_PASSWORD="postgres" \
-            -p $port:5432 \
-            $POSTGRES_IMAGE 2>&1)
-        set -e
-
-        if [[ $output != *"port is already allocated"* ]]; then
-            echo "PostgreSQL is running on port $port"
-            break
-        else
-            echo "Port $port is already in use. Trying next port..."
-            port=$((port + 1))
-        fi
-    done
-}
-
 run_psql() {
     # docker exec $TYPEORM_HOST psql -U postgres "$@"
     PGPASSWORD=postgres psql -h $TYPEORM_HOST -U postgres -w "$@"
-}
-
-wait_for_postgresql() {
-    count=0
-    until run_psql -c 'SELECT 1' >/dev/null 2>&1; do
-        sleep 1
-
-        count=$((count + 1))
-        echo "Waiting for PostgreSQL... $count seconds"
-    done
-
-    sleep 1
-
-    echo "PostgreSQL has started."
 }
 
 init_database() {
@@ -59,6 +18,4 @@ init_database() {
     run_psql -d $TYPEORM_DATABASE -c "GRANT ALL ON SCHEMA $TYPEORM_SCHEMA TO $TYPEORM_USERNAME WITH GRANT OPTION;"
 }
 
-start_postgresql
-wait_for_postgresql
 init_database
