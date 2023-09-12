1c067ce98f89ea99bef1fdf203cbb79b475bec19
hunjuly <hunjuly@gmail.com>
2023-09-12 06:55:51 +0000

    .

--- scripts/workflow.sh
+++
@@ -1,8 +0,0 @@
-- name: Create logs folder
-run: mkdir -p ./logs
-
-- name: set allow schema reset
-run: bash scripts/set_allow_schema_reset.sh
-
-- name: Init DB
-run: bash scripts/init_database.sh
