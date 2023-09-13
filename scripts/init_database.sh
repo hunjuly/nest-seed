9efed454e724b00bad85b7d5ad7efdce73acf2ab
hunjuly <hunjuly@gmail.com>
2023-09-13 15:21:07 +0000

    .

--- scripts/init_database.sh
+++ scripts/init_database.sh
@@ -10,11 +10,17 @@ run_psql() {
 
 wait_for_postgresql() {
     count=0
+    max_retries=10  # 최대 시도 횟수 설정
     until run_psql -c 'SELECT 1' >/dev/null 2>&1; do
         sleep 1
 
         count=$((count + 1))
         echo "Waiting for PostgreSQL... $count seconds"
+
+        if [ $count -gt $max_retries ]; then
+            echo "Error: Maximum retries exceeded. Exiting."
+            exit 1
+        fi
     done
 
     sleep 1
