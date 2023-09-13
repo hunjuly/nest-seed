1e481fe24c1ecd81817d9c59ad09ade6413cf84f
hunjuly <hunjuly@gmail.com>
2023-09-13 14:29:04 +0000

    .

--- scripts/init_github_workflow.sh
+++ scripts/init_github_workflow.sh
@@ -2,6 +2,7 @@
 set -e
 . "$(dirname "$0")"/common.cfg
 
+GITHUB_WORKSPACE
 bash $SCRIPTS_PATH/init_database.sh
 bash $SCRIPTS_PATH/set_allow_schema_reset.sh
 
@@ -9,17 +10,3 @@ cd ..
 
 mkdir -p logs
 npm install
-
-
---workdir /__w/nest-seed/nest-seed --network github_network_23c1ca40c98045209ecce3b246682720
--e "HOME=/github/home"
--e GITHUB_ACTIONS=true -e CI=true
--v "/var/run/docker.sock":"/var/run/docker.sock"
--v "/home/runner/work":"/__w"
--v "/home/runner/runners/2.308.0/externals":"/__e":ro
--v "/home/runner/work/_temp":"/__w/_temp"
--v "/home/runner/work/_actions":"/__w/_actions"
--v "/opt/hostedtoolcache":"/__t"
--v "/home/runner/work/_temp/_github_home":"/github/home"
--v "/home/runner/work/_temp/_github_workflow":"/github/workflow"
---entrypoint "tail" node:lts-alpine "-f" "/dev/null"
