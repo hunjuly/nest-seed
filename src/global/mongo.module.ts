41030970084cecf42af849266b248e4143c14703
hunjuly <hunjuly@gmail.com>
2023-10-11 13:33:26 +0000

    .

--- src/global/mongo.module.ts
+++ src/global/mongo.module.ts
@@ -8,7 +8,7 @@ import { ConfigModule } from './config.module'
         ConfigModule,
         MongooseModule.forRootAsync({
             useFactory: (configService: SafeConfigService) => {
-                const user = configService.getString('MONGO_USER')
+                const user = configService.getString('MONGO_USERNAME')
                 const pass = configService.getString('MONGO_PASSWORD')
                 const host = configService.getString('MONGO_HOST')
                 const port = configService.getNumber('MONGO_PORT')
