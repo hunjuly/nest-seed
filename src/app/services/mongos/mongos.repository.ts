288349819390720ee70e66a383a1155fabaeb079
hunjuly <hunjuly@gmail.com>
2023-12-23 14:44:34 +0000

    .

--- src/app/services/mongos/mongos.repository.ts
+++ src/app/services/mongos/mongos.repository.ts
@@ -12,44 +12,15 @@ export class MongosRepository extends MongooseRepository<Mongo> {
         super(model)
     }
 
-    async update2(id: string, updateMongoDto: UpdateMongoDto): Promise<MongoDocument> {
-        // 안전한 데이터만 필터링
-        const safeData = this.filterUpdateData(updateMongoDto)
-
-        const updatedDocument = await this.model
-            .findByIdAndUpdate(id, safeData, { returnDocument: 'after', upsert: false })
-            .exec()
-
-        return updatedDocument as MongoDocument
-    }
-
-    /* istanbul ignore next */
-    filterUpdateData(updateDto: UpdateMongoDto): Partial<UpdateMongoDto> {
+    async update(id: string, updateMongoDto: UpdateMongoDto): Promise<MongoDocument> {
         const safeData: Partial<UpdateMongoDto> = {}
+        safeData.name = updateMongoDto.name
+        safeData.desc = updateMongoDto.desc
+        safeData.date = updateMongoDto.date
+        safeData.enums = updateMongoDto.enums
+        safeData.integer = updateMongoDto.integer
 
-        // if (updateDto.name && typeof updateDto.name === 'string' && updateDto.name.length <= 100) {
-        // if (updateDto.name && typeof updateDto.name === 'string') {
-        safeData.name = updateDto.name
-        // }
-
-        // if (updateDto.desc && typeof updateDto.desc === 'string' && updateDto.desc.length <= 200) {
-        // if (updateDto.desc && typeof updateDto.desc === 'string') {
-        safeData.desc = updateDto.desc
-        // }
-
-        // if (updateDto.date && typeof updateDto.date === 'string') {
-        safeData.date = updateDto.date
-        // }
-
-        // if (Array.isArray(updateDto.enums)) {
-        safeData.enums = updateDto.enums // 여기에서 추가적인 검증을 적용할 수 있습니다.
-        // }
-
-        // if (typeof updateDto.integer === 'number') {
-        safeData.integer = updateDto.integer
-        // }
-
-        return safeData
+        return super.update(id, safeData)
     }
 
     async findByName(queryDto: MongosQueryDto): Promise<PaginationResult<MongoDocument>> {
