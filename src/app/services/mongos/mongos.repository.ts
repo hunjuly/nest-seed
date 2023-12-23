4a5318e518f0ccca5a9e76c347030b8c6e7e81a0
hunjuly <hunjuly@gmail.com>
2023-12-23 14:11:41 +0000

    .

--- src/app/services/mongos/mongos.repository.ts
+++ src/app/services/mongos/mongos.repository.ts
@@ -27,27 +27,27 @@ export class MongosRepository extends MongooseRepository<Mongo> {
     filterUpdateData(updateDto: UpdateMongoDto): Partial<UpdateMongoDto> {
         const safeData: Partial<UpdateMongoDto> = {}
 
-        // if (updateDto.name && typeof updateDto.name === 'string' && updateDto.name.length <= 100) {
-        // if (updateDto.name && typeof updateDto.name === 'string') {
-        safeData.name = updateDto.name
-        // }
+        if (updateDto.name && typeof updateDto.name === 'string' && updateDto.name.length <= 100) {
+            // if (updateDto.name && typeof updateDto.name === 'string') {
+            safeData.name = updateDto.name
+        }
 
-        // if (updateDto.desc && typeof updateDto.desc === 'string' && updateDto.desc.length <= 200) {
-        // if (updateDto.desc && typeof updateDto.desc === 'string') {
-        safeData.desc = updateDto.desc
-        // }
+        if (updateDto.desc && typeof updateDto.desc === 'string' && updateDto.desc.length <= 200) {
+            // if (updateDto.desc && typeof updateDto.desc === 'string') {
+            safeData.desc = updateDto.desc
+        }
 
-        // if (updateDto.date && typeof updateDto.date === 'string') {
-        if (updateDto.date) safeData.date = new Date(updateDto.date)
-        // }
+        if (updateDto.date && typeof updateDto.date === 'string') {
+            safeData.date = new Date(updateDto.date)
+        }
 
-        // if (Array.isArray(updateDto.enums)) {
-        safeData.enums = updateDto.enums // 여기에서 추가적인 검증을 적용할 수 있습니다.
-        // }
+        if (Array.isArray(updateDto.enums)) {
+            safeData.enums = updateDto.enums // 여기에서 추가적인 검증을 적용할 수 있습니다.
+        }
 
-        // if (typeof updateDto.integer === 'number') {
-        safeData.integer = updateDto.integer
-        // }
+        if (typeof updateDto.integer === 'number') {
+            safeData.integer = updateDto.integer
+        }
 
         return safeData
     }
