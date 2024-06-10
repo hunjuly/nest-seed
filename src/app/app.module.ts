f3238b9090d45ec76c4e7fea9b4adc1eb960c418
hunjuly <hunjuly@gmail.com>
2024-06-10 07:32:09 +0000

    fix compile errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,13 @@
 import { Module } from '@nestjs/common'
-import {
-    AuthController,
-    CustomersController,
-    MongolsController,
-    MoviesController,
-    PsqlsController,
-    UsersController
-} from 'app/controllers'
+import { AuthController, CustomersController, MoviesController, UsersController } from 'app/controllers'
 import { GlobalModule } from 'app/global'
 import { AuthModule } from './services/auth'
 import { CustomersModule } from './services/customers'
-import { MongolsModule } from './services/mongols'
 import { MoviesModule } from './services/movies'
-import { PsqlsModule } from './services/psqls'
 import { UsersModule } from './services/users'
 
 @Module({
-    imports: [
-        GlobalModule,
-        CustomersModule,
-        MoviesModule,
-        AuthModule,
-        UsersModule,
-        PsqlsModule,
-        MongolsModule
-    ],
-    controllers: [
-        CustomersController,
-        MoviesController,
-        UsersController,
-        AuthController,
-        PsqlsController,
-        MongolsController
-    ]
+    imports: [GlobalModule, CustomersModule, MoviesModule, AuthModule, UsersModule],
+    controllers: [CustomersController, MoviesController, UsersController, AuthController]
 })
 export class AppModule {}
