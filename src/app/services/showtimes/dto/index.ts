125c34e4698675db013b01ddad715e5dca0f548d
hunjuly <hunjuly@gmail.com>
2024-06-17 21:39:04 +0000

    .

--- src/app/services/showtimes/dto/index.ts
+++ src/app/services/showtimes/dto/index.ts
@@ -1,4 +1,4 @@
+export * from './showtime.dto'
 export * from './showtimes-creation.request'
 export * from './showtimes-creation.result'
-export * from './showtime.dto'
 export * from './showtimes-query.dto'
