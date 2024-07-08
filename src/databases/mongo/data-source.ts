6ce7ab51e055af0a13d6ef2e16439c85fc96869e
hunjuly <hunjuly@gmail.com>
2024-07-08 10:28:00 +0000

    .

--- src/databases/mongo/data-source.ts
+++ src/databases/mongo/data-source.ts
@@ -3,7 +3,7 @@ import { mongoOptions } from 'config'
 
 const { user, pass, host1, host2, host3, port, replica, database: dbName } = mongoOptions
 
-notUsed(host3, '3개를 다 적을 필요는 없다F')
+notUsed(host3, '3개를 다 적을 필요는 없다')
 const uri = `mongodb://${user}:${pass}@${host1}:${port},${host2}:${port}/?replicaSet=${replica}`
 
 export const mongoDatasource = { uri, dbName }
