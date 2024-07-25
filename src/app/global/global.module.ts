c76a6d7752650fb9addab573d09ad7219b745af1
hunjuly <hunjuly@gmail.com>
2024-07-25 21:17:29 +0000

    remove psql module

--- src/app/global/global.module.ts
+++ src/app/global/global.module.ts
@@ -4,18 +4,9 @@ import { EventModule } from './event.module'
 import { HttpModule } from './http.module'
 import { LoggerModule } from './logger.module'
 import { MongoDbModule } from './mongo.db.module'
-import { PsqlDbModule } from './psql.db.module'
 import { QueueModule } from './queue.module'
 
 @Module({
-    imports: [
-        CacheModule,
-        EventModule,
-        QueueModule,
-        HttpModule,
-        LoggerModule,
-        MongoDbModule,
-        PsqlDbModule
-    ]
+    imports: [CacheModule, EventModule, QueueModule, HttpModule, LoggerModule, MongoDbModule]
 })
 export class GlobalModule {}
